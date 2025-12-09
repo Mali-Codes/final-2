eaf9efb73cb45397a27f0ea7a2f241e6dc57fabd
Malakai <poncemalakai@gmail.com>
2025-12-08 21:05:22 -0800

    working on milestone 1

--- Final2.cpp
+++ Final2.cpp
@@ -6,14 +6,29 @@
 #include <ctime>
 using namespace std;
 
+struct Order {
+    string customer;
+    string drink;
+    Order* next;
 
+    Order(string c, string d) : customer(c), drink(d), next(nullptr) {}
+};
 
-
-
-
-
-
-
-int main {
+class CoffeeShop {
+    private:
     
+}
+
+
+
+
+
+
+int main() {
+    srand(time(0));
+
+    string coffeeNames[] = {"Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Henry"};
+    string coffeeDrinks[] = {"Espresso", "Latte", "Cappuccino", "Americano", "Mocha", "Macchiato"};
+
+
 }
