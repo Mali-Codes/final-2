7df2a48f3264bc3ab24f00549af9bb9eb1009d23
Malakai <poncemalakai@gmail.com>
2025-12-08 22:31:20 -0800

    working on milestone 4

--- Final2.cpp
+++ Final2.cpp
@@ -141,57 +141,7 @@ int main() {
     }
     
     
-    
-
-    // for (int i = 0; i < initalSize; i++) { //from lab 32
-    //     Car c;
-    //     tollbooth.push_back(c);
-    // }
-
-    // cout << "Start queue" << endl;
-    
-    // for (auto& car : tollbooth) {
-    //     cout << "    ";
-    //     car.print();
-    // }
-
-
-    // int time = 1;
-
-    // while (!tollbooth.empty()) {
-    //     cout << "\nTime " << time << ":\n";
-
-    //     int action = rand() % 2;
-
-    //     if (action == 0) {
-    //         Car newCar;
-    //         tollbooth.push_back(newCar);
-    //         cout << "    New car arrived: ";
-    //         newCar.print();
-    //     }
-    //     else {
-    //         cout << "Paid toll: ";
-    //         tollbooth.front().print();
-    //         tollbooth.pop_front(); // take it out 
-    //     }
-
-    //     cout << "Queue: \n";
-
-    //     if (tollbooth.empty()) {
-    //         cout << "empty queue \n";
-    //     } else {
-    //         for (auto& car : tollbooth) {
-    //             cout << "    ";
-    //             car.print();
-    //         }
-    //     }
-        
-    //     time ++;
-    
-    // }
-
-    // return 0;
-
+ 
 
 
 
